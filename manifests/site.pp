a5bea8bcdcf315e9458c75b2d34eeee93acd682e
Matias Moglia <matias.moglia@noname-it.com.ar>
2021-12-03 12:43:36 -0300

    new code to test deployment

--- manifests/site.pp
+++ manifests/site.pp
@@ -27,5 +27,5 @@ File { backup => false }
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
-  #   class { 'my_class': }
+  notify { 'this is the default': }
 }
