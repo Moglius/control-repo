af4f2d3d2fe0f53688bd618e5d0ce9b6f40688fe
Matias Moglia <matias.moglia@noname-it.com.ar>
2021-12-05 11:01:32 -0300

    adding vhost to data profile

--- site-modules/profile/manifests/apache.pp
+++ site-modules/profile/manifests/apache.pp
@@ -3,9 +3,8 @@ class profile::apache {
   include apache
 
   apache::vhost {'project1':
-    port          => $apache::port,
-    vhost_name    => $apache::vhost_name,
-    document_root => $apache::document_root,
+    vhost_name    => 'project1.example.com',
+    document_root => '/var/www/project1',
   }
 
 }
